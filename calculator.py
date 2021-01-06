40d8b24823edb63ec5af44e20de70e3f82e0b337
Christina <crissygardner@gmail.com>
2021-01-06 21:42:06 +0000

    test

---
+++ calculator.py
@@ -0,0 +1,20 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
+"""
+have 3 parameters as inputs 
+    operand
+    num1
+    num2
+
+tokenize based on spaces ' '
+if it's just one parameter "q" it quits the program
+
+after we split it we write our funtions for each of the conditional statements.
+"""
+initial_string = input(f"Enter prefix calculation: ")
+
